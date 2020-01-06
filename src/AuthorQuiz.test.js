6fd911c7fac6a367c5fe29a13330c9df26ebfbf1
Polina <p.moysenia@gmail.com>
2020-01-06 17:10:02 +0300

    Tests cases added

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -15,13 +15,63 @@ const state = {
       books: ['One']
     }
   },
-  highlight: 'none',
-  onAnswerSelected: () => {}
+  highlight: 'none'
 }
 
 describe('Author Quiz', () => {
   it("renders without crashing", ()=>{
       const div = document.createElement("div");
-      ReactDOM.render(<AuthorQuiz {...state}/>, div);
+      ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={()=>{}}/>, div);
+  });
+
+  describe("when no answer has selected", () => {
+    let wrapper;
+    beforeAll(()=>{
+      wrapper = mount(<AuthorQuiz {...state} onAnswerSelected={()=>{}}/>);
+    });
+
+    it("should have no background color", () => {
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe('');
+    })
+  });
+  
+  describe("when wrong answer has selected", () => {
+    let wrapper;
+    beforeAll(()=>{
+      wrapper = mount(<AuthorQuiz {...(Object.assign({}, state, {highlight: 'wrong'}))} onAnswerSelected={()=>{}}/>);
+    });
+
+    it("should have red background color", () => {
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe('red');
+    })
+  });
+
+  describe("when correct answer has selected", () => {
+    let wrapper;
+    beforeAll(()=>{
+      wrapper = mount(<AuthorQuiz {...(Object.assign({}, state, {highlight: 'correct'}))} onAnswerSelected={()=>{}}/>);
+    });
+
+    it("should have green background color", () => {
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe('green');
+    });
+  });
+
+  describe("when the first answer has selected", () => {
+    let wrapper;
+    const handleAnswerSelected = jest.fn();
+
+    beforeAll(()=>{
+      wrapper = mount(<AuthorQuiz {...state} onAnswerSelected={handleAnswerSelected}/>);
+      wrapper.find('.answer').first().simulate('click');
+    });
+
+    it("onAnswerSelected should be called", () => {
+      expect(handleAnswerSelected).toHaveBeenCalled();
+    });
+
+    it("should receive first titile", () => {
+      expect(handleAnswerSelected).toHaveBeenCalledWith("One");
+    });
   });
 })
