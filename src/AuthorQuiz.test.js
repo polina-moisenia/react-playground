969e33a400fc75ad09f6e9979f06beee83073672
Polina <p.moysenia@gmail.com>
2020-01-06 16:44:05 +0300

    Initial test added

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom'
 import AuthorQuiz from './AuthorQuiz';
+import Enzyme, {mount, shallow, render} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+Enzyme.configure({ adapter : new Adapter()})
 
-test('renders learn react link', () => {
-  const { getByText } = render(<AuthorQuiz />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const state = {
+  turnData : {
+    books : ['One', 'Two', 'Three'],
+    author : {
+      name: 'Name',
+      imageUrl: 'some_url',
+      imageSource: 'source',
+      books: ['One']
+    }
+  },
+  highlight: 'none',
+  onAnswerSelected: () => {}
+}
+
+describe('Author Quiz', () => {
+  it("renders without crashing", ()=>{
+      const div = document.createElement("div");
+      ReactDOM.render(<AuthorQuiz {...state}/>, div);
+  });
+})
